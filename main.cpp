5d4fa8f36a92c5f03f46598357ae2f98674a0712
Definition of Dean <defofdean@gmail.com>
2019-04-25 16:46:27 +0100

    Trial wide strings

--- main.cpp
+++ main.cpp
@@ -1,50 +1,39 @@
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <iterator>
+#include <sstream>
 #include <vector>
 
-const std::vector<std::pair<double, double>> example{
-  {0, 20388},
-{1, 19369},
-{2, 32197},
-{3, 8933},
-{4, 5793},
-{5, 17980},
-{6, 10969},
-{7, 8222},
-{8, 19157},
-{9, 6290},
-{10, 20243},
-{11, 9290},
-{12, 7107},
-{13, 22798},
-{14, 23009},
-{15, 31328},
-{16, 20245},
-{17, 11224},
-{18, 32428},
-{19, 30104},
-{20, 901},
-{21, 9478},
-{22, 21044},
-{23, 2971},
-{24, 6916},
-{25, 18130},
-{26, 4172},
-{27, 3469},
-{28, 14083},
-{29, 16533},
-{30, 8751},
-{31, 19906},
-{32, 14732},
-{33, 11222},
-{34, 3095},
-{35, 20529},
-{36, 29188},
-{37, 29070},
-{38, 23777},
-{39, 17660},
-{40, 8122},
+const std::vector<double> example{
+    15147, 2017,  26749, 3862,  2529,  19151, 28393, 13840, 1589,  785,   22937,
+    20649, 13643, 30358, 7337,  12124, 19168, 29311, 4191,  24413, 30185, 13786,
+    5016,  3991,  16232, 24969, 6851,  13793, 29429, 13119, 31250, 29336, 6312,
+    26219, 8665,  20804, 24777, 23842, 5087,  8119,  10590,
 };
 
+// Create text representation of basic histogram
+using iterator_t = std::vector<double>::const_iterator;
+std::wstring dump_histogram(const iterator_t &begin, const iterator_t &end) {
+
+  // Calculate max bin so we can scale the output
+  const double max_bin{*std::max_element(begin, end)};
+
+  // Max width of a bar
+  const size_t max_bar_length = 70;
+
+  std::wstringstream out;
+  out << '\n';
+  std::for_each(begin, end, [&](const auto &bin) {
+    out << std::wstring(1 + std::rint(max_bar_length * bin / max_bin), L'⣿')
+        << "\033[36m_\033[0m\n";
+  });
+
+  return out.str();
+}
+
 int main() {
-  std::cout << example.size() << " entries\n";
+  std::wcout << example.size() << " entries\n"
+             << dump_histogram(std::cbegin(example), std::cend(example))
+             << '\n';
 }
--- makefile
+++ makefile
@@ -3,12 +3,12 @@ CXX = g++-8
 FLAGS = --std=c++2a --all-warnings --extra-warnings -Wno-address \
 	-Werror -Wshadow -Wfloat-equal -Weffc++ -Wdelete-non-virtual-dtor -O1
 
-all: tmp tmp/agraph
+all: tmp tmp/agraph noise
 
 tmp/%.o: %.cpp
 	$(CXX) $(FLAGS) -c -o $@ $<
 
-tmp/agraph: tmp/main.o tmp/ascii.o
+tmp/agraph: tmp/main.o
 	$(CXX) $(FLAGS) -o $@ $^
 
 tmp:
@@ -16,3 +16,7 @@ tmp:
 
 clean:
 	rm -rf tmp
+
+noise:
+	tmp/agraph
+
