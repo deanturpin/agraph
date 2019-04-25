ebc4ff37cd12d84f795f5c2dd1aab203ac239381
Definition of Dean <defofdean@gmail.com>
2019-04-25 18:42:38 +0100

    Add unit test

--- main.cpp
+++ main.cpp
@@ -31,15 +31,15 @@ std::string dump_histogram(const iterator_t &begin, const iterator_t &end) {
 }
 
 int main() {
-  assert(dump_histogram(std::cbegin(input), std::cend(input)) == output);
 
-  std::cout << "hello\n";
+  // Unit test me
+  assert(dump_histogram(std::cbegin(input), std::cbegin(input)) == "" &&
+         "Two copies of the same iterator should return empty string");
+  assert(dump_histogram(std::cbegin(input), std::cend(input)) == output);
 
   // Read all the values
   const std::vector<double> bins{std::istream_iterator<double>(std::cin), {}};
 
   // Report
   std::cout << dump_histogram(std::cbegin(bins), std::cend(bins)) << '\n';
-
-  std::cout << "goodbye\n";
 }
