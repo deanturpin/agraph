861e637e23c510f276716cb8fefa224f392a813e
Definition of Dean <defofdean@gmail.com>
2019-05-02 21:46:39 +0000

    Add offset for negative values

--- draw.cpp
+++ draw.cpp
@@ -16,16 +16,16 @@ void plot(const double &v) {
   // On first call get terminal width from environment or use default
   const static size_t max_bar_length = [] {
     if (const char *cols = std::getenv("COLUMNS"))
-      return std::stoul(cols);
+      return std::stoul(cols) / 2;
     else
-      return 80ul;
+      return 40ul;
   }();
 
-  const size_t bar_length = std::rint(max_bar_length * (v - min) / range);
+  const size_t bar_length = std::rint(max_bar_length +( max_bar_length * (v - min) / range));
 
   // Print bar
-  std::wcout << std::wstring(bar_length, '-') << '\n';
+  std::wcout << std::wstring(bar_length, ' ') << "|\n";
 
   using namespace std::chrono_literals;
-  std::this_thread::sleep_for(20ms);
+  std::this_thread::sleep_for(10ms);
 }
