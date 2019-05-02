d710f6eb1512d736faec79033494483a6acab8fe
Definition of Dean <defofdean@gmail.com>
2019-05-02 19:39:22 +0100

    Remove commented code, simplify into single line read and plot

--- main.cpp
+++ main.cpp
@@ -11,44 +11,18 @@ int main(int argc, char **argv) {
   std::ifstream file{argv[1]};
   auto &in = argc > 1 ? file : std::cin;
 
-  // Create container for incoming values
-  // std::vector<double> frame;
-
-  // Read values until we have a full frame
-  // const size_t max_frame{1024};
-
+  // Extract lines until end of file
   std::string line;
   while (std::getline(in, line)) {
 
+    // Push line into a string stream
     std::stringstream ss;
     ss.precision(10);
     ss << line;
 
+    // If we can extract a double then print it
     double v{};
     if (ss >> v)
       plot(v);
-    else
-      std::cout << "skip\n";
-
-    // if (in >> v) {
-    //   std::cout << "store " << v << "\n";
-
-    //   // Store value
-    //   frame.push_back(v);
-
-    //   // Draw frame if we've exceeded frame size
-    //   if (frame.size() >= max_frame) {
-    //     draw_histogram(std::cbegin(frame), std::cend(frame));
-    //     frame.clear();
-    //   }
-    // }
-    // else
-    //   std::cout << "skipping\n";
   }
-
-  // std::cout << frame.size() << " samples\n";
-
-  // If there's anything left then draw it
-  // if (!frame.empty())
-  //   draw_histogram(std::cbegin(frame), std::cend(frame));
 }
